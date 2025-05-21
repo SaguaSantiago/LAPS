3b93fc602973d1c5e61e5da9542bb669dd7fde3f
saguasantiago <saguasantiago@gmail.com>
2025-05-21 11:25:52 -0300

    category model updated with associations

--- models/category.rb
+++ models/category.rb
@@ -1,4 +1,13 @@
 class Category < ActiveRecord::Base
   validates :name, presence: true
   validates :description, presence: true
+
+  belongs_to :account
+  has_many :transactions
+  has_many :events
+
 end
+
+# belongs_to :user
+# has_many :source_transactions, 
+# class_name: 'Transaction', foreign_key: :source_account_id
