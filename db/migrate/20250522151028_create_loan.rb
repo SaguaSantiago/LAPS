b24d1e2b5a60180e4af1a6d39da3d8b687786682
LucasLillo03 <lucaslillo0207@gmail.com>
2025-05-22 13:21:05 -0300

    loan model and table created

---
+++ db/migrate/20250522151028_create_loan.rb
@@ -0,0 +1,11 @@
+class CreateLoan < ActiveRecord::Migration[8.0]
+  def change
+    create_table :loan do |t|
+      t.integer :quotas_number
+      t.date :expiration_period
+      t.decimal :interest, precision: 5, scale: 2
+      t.reference :transaction, foreign_key: true #relacion con transaction
+
+      t.timestamps
+  end
+end
