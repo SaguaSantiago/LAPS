028b09981dbdda0d8d345ba4b55feb86ed9d7b62
palomabrizuela <palomabrizuela19@gmail.com>
2025-05-23 08:57:58 -0300

    Add models and migrations for quota and fixed some errors

--- db/migrate/20250521141028_add_category_associations.rb
+++ db/migrate/20250521141028_add_category_associations.rb
@@ -1,7 +1,7 @@
-class AddCategoryAssociations < ActiveRecord::Migration[8.0]
+class AddCategoryAssociations < ActiveRecord::Migration[7.0]
   def change
-    add_reference :categorys, :account, foreign_key: true
-    add_reference :categorys, :event, foreign_key: true
-    add_reference :categorys, :transaction, foreign_key: true
+    add_reference :categories, :account, foreign_key: true
+    add_reference :categories, :event, foreign_key: true
+    add_reference :categories, :transaction, foreign_key: true
   end
 end
