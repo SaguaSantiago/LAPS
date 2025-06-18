5fc355eae68a30b9bf3501f246e2490700ff88f0
SaguaSantiago <saguasantiago@gmail.com>
2025-06-18 18:05:04 -0300

    calendar view

--- models/event.rb
+++ models/event.rb
@@ -1,4 +1,9 @@
 class Event < ActiveRecord::Base
   has_many :event_schedule
   has_many :event_dates, through: :event_schedule
+  belongs_to :category
+  belongs_to :account
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :period, presence: true, inclusion: { in: %w[monthly diary spontaneus] }
 end
