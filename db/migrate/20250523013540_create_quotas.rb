cdb40a40873fd48f7f4821f0a8728b7bcc3e80ad
palomabrizuela <palomabrizuela19@gmail.com>
2025-06-19 17:43:44 -0300

    changed quota migration, loan and quota model

--- db/migrate/20250523013540_create_quotas.rb
+++ db/migrate/20250523013540_create_quotas.rb
@@ -1,10 +1,11 @@
 class CreateQuotas < ActiveRecord::Migration[7.0]
   def change
     create_table :quotas do |t|
-      t.references :loans, null: false, foreign_key: true
+      t.references :transaction, null: false, foreign_key: true  # usamos 'transaction'
       t.integer :number
       t.boolean :state, default: false
       t.float :quota_mount
+      t.timestamps
       t.timestamps
     end
   end
--- models/loan.rb
+++ models/loan.rb
@@ -1,11 +1,12 @@
 class Loan < Transaction
     belongs_to :source_account, class_name: 'Account'
     belongs_to :target_account, class_name: 'Account'
-    has_many :quotas, class_name: 'Quota', dependent: :destroy
+    has_many :quotas, class_name: 'Quota', foreign_key: 'transaction_id', dependent: :destroy
 
     validates :expiration_period, presence: true
     validates :quotas_number, presence: true, numericality: { greater_than: 0 }
-    
+    class Loan < Transaction
+
     #Callback para que cuando se cree el préstamo, se acredite el dinero a la cuenta destino
     after_create :apply_loan
     private
@@ -13,6 +14,16 @@ class Loan < Transaction
     	ActiveRecord::Base.transaction do
             target_account.balance += amount
             target_account.save!
+            # Calcular monto de cada cuota
+      	    cuota_monto = (amount / quotas_number).round(2)
+      
+      	    quotas_number.times do |i|
+            quotas.create!(
+            number: i + 1,
+            quota_mount: cuota_monto,
+            state: false
+            )
     	end
     end
 end
+
